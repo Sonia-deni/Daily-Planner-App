c8d37860fceaf7725888e2ebb8c417b1b5804110
sonia-deni <sonia_deni@hotmail.com>
2023-01-20 22:11:38 +0000

    created a loop to check the current time against the time of each textarea

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-
 var timeDisplayEl = $("#currentDay");
-var today = moment();
+//var today = moment();
 
-setInterval(displayTime, 1000);
+setInterval(displayTime, 10000);
 function displayTime() {
     var currentTime = moment().format("DD MMM YYYY");
     timeDisplayEl.text(currentTime);
+
+   
+    const rows = document.getElementsByClassName("description"); //gets an array of all the textareas
+    var currentHour = parseInt(moment().format('H')); //get the current hour to compare with the ids of each textarea
+
+    for(var i=0; i<rows.length; i++){
+        var hourToCompare = rows[i].id;
+        console.log(hourToCompare);
+        if(hourToCompare === currentHour){
+            console.log("current hour" + currentHour);
+        }
+        else if(hourToCompare > currentHour){
+           console.log(hourToCompare + " is after " + currentHour);
+        }
+        else if(hourToCompare < currentHour){
+            console.log(hourToCompare + " is before " + currentHour);
+        }
+    }
+
+
 }
+    
