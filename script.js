b54c2050d8730f00e0e8cb8714cba6dc467ee77e
sonia-deni <sonia_deni@hotmail.com>
2023-01-21 12:37:43 +0000

    function to handle save button click, get the corresponding text input and then create an object

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 var timeDisplayEl = $("#currentDay");
-//var today = moment();
 
-setInterval(displayTime, 1000);
-function displayTime() {
+setInterval(displayTime, 1000); 
+function displayTime() {//updates the time and colour codes the timeblocks
     var currentTime = moment().format("DD MMM YYYY");
     timeDisplayEl.text(currentTime);
 
-   
     const rows = document.getElementsByClassName("description"); //gets an array of all the textareas
     var currentHour = parseInt(moment().format('H')); //get the current hour to compare with the ids of each textarea
 
     for(var i=0; i<rows.length; i++){
         var hourToCompare = rows[i].id;
         var setColor = rows[i];
-        console.log(hourToCompare);
         //compare the hours, and set colours accordingly
         if(hourToCompare > currentHour){
-           console.log(hourToCompare + " is after " + currentHour);
            setColor.setAttribute('class','future col-10 description');
         }
         else if(hourToCompare < currentHour){
-            console.log(hourToCompare + " is before " + currentHour);
             setColor.setAttribute('class','past col-10 description');
         }
         else{
             setColor.setAttribute('class','present col-10 description');
         }
-
-       
     }
 
 
 }
     
+$('.saveBtn').on('click', function(){
+//get the value of the textbox with the matching id. If no text, alert
+    var hourSaved = parseInt(this.id);    
+    var textToSave = getText(hourSaved);
+    if(textToSave){
+        var saveObject = {
+            time: hourSaved,
+            description: textToSave
+        }
+        console.log(saveObject);
+    }
+    else{
+       alert("no text input");
+    }
+    
+});
+
+function getText(hourClicked){ //finds the text input for the corresponding save button and returns it
+    var hour = hourClicked;
+    var textInputs = $('.description');
+
+    for(var i=0; i<textInputs.length; i++){
+        var matchTextHour = parseInt(textInputs[i].id);
+        if(matchTextHour === hour){
+            return textInputs[i].value;
+        }
+    }
+}
+
+
